d68bf323326ed5cba4e28107dec9d5855aa1be60
mi-reu <dev.mireu@gmail.com>
2025-04-12 22:26:32 +0900

    feat: react-speech-recognition 제거

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,23 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+import { useState } from "react";
+
 import VoiceChatModeButton from "./_components/VoiceChatModeButton";
 import UserMessage from "./_components/UserMessage";
 import AiMessage from "./_components/AiMessage";
 
 export default function Home() {
-  const { transcript, listening, browserSupportsSpeechRecognition } =
-    useSpeechRecognition();
-
   const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!browserSupportsSpeechRecognition) {
-      alert("브라우저가 음성 인식을 지원하지 않습니다.");
-    }
-  }, [browserSupportsSpeechRecognition]);
 
   return (
     <div className="relative min-h-screen flex flex-col">
@@ -26,24 +15,14 @@ export default function Home() {
           {/* AI 메시지 */}
           <AiMessage message="안녕하세요! 대출 상담을 도와드리겠습니다. 어떤 도움이 필요하신가요?" />
 
-          {transcript.length > 0 && (
-            <>
-              {/* 사용자 메시지 */}
-              <UserMessage message={transcript} />
-            </>
-          )}
+          {/* 사용자 메시지 */}
+          <UserMessage message="대출 상담을 받고 싶어." />
         </div>
       </div>
       <VoiceChatModeButton
         className="fixed bottom-8 left-1/2 -translate-x-1/2"
-        loading={listening}
+        loading={isLoading}
         onClick={() => {
-          if (!isLoading) {
-            SpeechRecognition.startListening({ language: "ko" });
-          } else {
-            SpeechRecognition.stopListening();
-          }
-
           setIsLoading(!isLoading);
         }}
       />
